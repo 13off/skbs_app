87d1ebf565a504bf2c3d14406b5ae2092786f6b7
13off <ilyaodincev1999@gmail.com>
2026-07-10 20:11:50 +0300

    добавлена плавная прокрутка без android эффекта

---
+++ lib/app/premium_scroll_behavior.dart
@@ -0,0 +1,32 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class PremiumScrollBehavior extends MaterialScrollBehavior {
+  const PremiumScrollBehavior();
+
+  @override
+  Set<PointerDeviceKind> get dragDevices => const {
+    PointerDeviceKind.touch,
+    PointerDeviceKind.mouse,
+    PointerDeviceKind.trackpad,
+    PointerDeviceKind.stylus,
+    PointerDeviceKind.unknown,
+  };
+
+  @override
+  ScrollPhysics getScrollPhysics(BuildContext context) {
+    return const BouncingScrollPhysics(
+      parent: AlwaysScrollableScrollPhysics(),
+    );
+  }
+
+  @override
+  Widget buildOverscrollIndicator(
+    BuildContext context,
+    Widget child,
+    ScrollableDetails details,
+  ) {
+    return child;
+  }
+}
