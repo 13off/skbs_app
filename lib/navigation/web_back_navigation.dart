5f437fbdcd215bfd098cb328ec7bcf9f456c1598
13off <ilyaodincev1999@gmail.com>
2026-07-10 12:54:02 +0300

    откат свайпа к быстрой версии

--- lib/navigation/web_back_navigation.dart
+++ lib/navigation/web_back_navigation.dart
@@ -17,19 +17,21 @@ class AppBrowserBackBridge extends StatefulWidget {
 }
 
 class _AppBrowserBackBridgeState extends State<AppBrowserBackBridge> {
-  static const double _edgeWidth = 28;
+  static const double _edgeWidth = 34;
   static const double _minSwipeDistance = 72;
 
   double _startX = 0;
   double _startY = 0;
   double _deltaX = 0;
   double _deltaY = 0;
+  bool _isEdgeSwipe = false;
 
   void _resetSwipe() {
     _startX = 0;
     _startY = 0;
     _deltaX = 0;
     _deltaY = 0;
+    _isEdgeSwipe = false;
   }
 
   bool _canPop() {
@@ -46,49 +48,34 @@ class _AppBrowserBackBridgeState extends State<AppBrowserBackBridge> {
     navigator.pop();
   }
 
-  bool get _isBackSwipe {
-    return _deltaX >= _minSwipeDistance &&
-        _deltaX.abs() > _deltaY.abs() * 1.35;
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      fit: StackFit.expand,
-      children: [
-        widget.child,
-        Positioned(
-          left: 0,
-          top: 0,
-          bottom: 0,
-          width: _edgeWidth,
-          child: GestureDetector(
-            behavior: HitTestBehavior.translucent,
-            onHorizontalDragStart: (details) {
-              _resetSwipe();
+    return GestureDetector(
+      behavior: HitTestBehavior.translucent,
+      onHorizontalDragStart: (details) {
+        _resetSwipe();
 
-              if (!_canPop()) return;
+        _startX = details.globalPosition.dx;
+        _startY = details.globalPosition.dy;
+        _isEdgeSwipe = _startX <= _edgeWidth && _canPop();
+      },
+      onHorizontalDragUpdate: (details) {
+        if (!_isEdgeSwipe) return;
 
-              _startX = details.globalPosition.dx;
-              _startY = details.globalPosition.dy;
-            },
-            onHorizontalDragUpdate: (details) {
-              if (!_canPop()) return;
+        _deltaX = details.globalPosition.dx - _startX;
+        _deltaY = details.globalPosition.dy - _startY;
+      },
+      onHorizontalDragEnd: (_) {
+        if (_isEdgeSwipe &&
+            _deltaX >= _minSwipeDistance &&
+            _deltaX.abs() > _deltaY.abs() * 1.35) {
+          _popOneScreen();
+        }
 
-              _deltaX = details.globalPosition.dx - _startX;
-              _deltaY = details.globalPosition.dy - _startY;
-            },
-            onHorizontalDragEnd: (_) {
-              if (_canPop() && _isBackSwipe) {
-                _popOneScreen();
-              }
-
-              _resetSwipe();
-            },
-            onHorizontalDragCancel: _resetSwipe,
-          ),
-        ),
-      ],
+        _resetSwipe();
+      },
+      onHorizontalDragCancel: _resetSwipe,
+      child: widget.child,
     );
   }
 }
