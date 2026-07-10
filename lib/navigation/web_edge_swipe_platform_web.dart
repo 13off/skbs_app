c5980214c705422a74c1a62588c1dece345c3e0a
13off <ilyaodincev1999@gmail.com>
2026-07-10 13:32:24 +0300

    добавлена защита от двойного свайпа

--- lib/navigation/web_edge_swipe_platform_web.dart
+++ lib/navigation/web_edge_swipe_platform_web.dart
@@ -4,13 +4,25 @@ import 'dart:html' as html;
 typedef EdgeSwipeBackCallback = void Function();
 
 class WebEdgeSwipePlatform {
+  static const int _minimumEventGapMs = 450;
+
   static EdgeSwipeBackCallback? _onBack;
   static StreamSubscription<html.Event>? _subscription;
+  static DateTime? _lastBackAt;
 
   static void initialize(EdgeSwipeBackCallback onBack) {
     _onBack = onBack;
 
     _subscription ??= html.window.on['appstroy-swipe-back'].listen((_) {
+      final now = DateTime.now();
+      final lastBackAt = _lastBackAt;
+
+      if (lastBackAt != null &&
+          now.difference(lastBackAt).inMilliseconds < _minimumEventGapMs) {
+        return;
+      }
+
+      _lastBackAt = now;
       _onBack?.call();
     });
   }
@@ -19,5 +31,6 @@ class WebEdgeSwipePlatform {
     _subscription?.cancel();
     _subscription = null;
     _onBack = null;
+    _lastBackAt = null;
   }
 }
