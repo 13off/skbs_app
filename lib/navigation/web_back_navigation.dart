e29bb18a114699a2a949b8781e4011ca6c3f27d2
13off <ilyaodincev1999@gmail.com>
2026-07-10 13:31:40 +0300

    подключен чистый edge swipe без history

--- lib/navigation/web_back_navigation.dart
+++ lib/navigation/web_back_navigation.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/widgets.dart';
 
+import 'web_edge_swipe_platform_stub.dart'
+    if (dart.library.html) 'web_edge_swipe_platform_web.dart';
+
 final GlobalKey<NavigatorState> appNavigatorKey = GlobalKey<NavigatorState>();
 
 class AppWebHistoryObserver extends NavigatorObserver {
   // Browser history больше не трогаем.
-  // На мобильном вебе это давало долгую перерисовку после свайпа.
+  // Свайп назад приходит из web/index.html отдельным событием.
 }
 
 class AppBrowserBackBridge extends StatefulWidget {
@@ -17,27 +20,16 @@ class AppBrowserBackBridge extends StatefulWidget {
 }
 
 class _AppBrowserBackBridgeState extends State<AppBrowserBackBridge> {
-  static const double _edgeWidth = 34;
-  static const double _minSwipeDistance = 72;
-
-  double _startX = 0;
-  double _startY = 0;
-  double _deltaX = 0;
-  double _deltaY = 0;
-  bool _isEdgeSwipe = false;
-
-  void _resetSwipe() {
-    _startX = 0;
-    _startY = 0;
-    _deltaX = 0;
-    _deltaY = 0;
-    _isEdgeSwipe = false;
+  @override
+  void initState() {
+    super.initState();
+    WebEdgeSwipePlatform.initialize(_popOneScreen);
   }
 
-  bool _canPop() {
-    final navigator = appNavigatorKey.currentState;
-
-    return navigator != null && navigator.canPop();
+  @override
+  void dispose() {
+    WebEdgeSwipePlatform.dispose();
+    super.dispose();
   }
 
   void _popOneScreen() {
@@ -49,33 +41,5 @@ class _AppBrowserBackBridgeState extends State<AppBrowserBackBridge> {
   }
 
   @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      behavior: HitTestBehavior.translucent,
-      onHorizontalDragStart: (details) {
-        _resetSwipe();
-
-        _startX = details.globalPosition.dx;
-        _startY = details.globalPosition.dy;
-        _isEdgeSwipe = _startX <= _edgeWidth && _canPop();
-      },
-      onHorizontalDragUpdate: (details) {
-        if (!_isEdgeSwipe) return;
-
-        _deltaX = details.globalPosition.dx - _startX;
-        _deltaY = details.globalPosition.dy - _startY;
-      },
-      onHorizontalDragEnd: (_) {
-        if (_isEdgeSwipe &&
-            _deltaX >= _minSwipeDistance &&
-            _deltaX.abs() > _deltaY.abs() * 1.35) {
-          _popOneScreen();
-        }
-
-        _resetSwipe();
-      },
-      onHorizontalDragCancel: _resetSwipe,
-      child: widget.child,
-    );
-  }
+  Widget build(BuildContext context) => widget.child;
 }
