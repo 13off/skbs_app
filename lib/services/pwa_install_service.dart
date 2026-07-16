fa1cab1a1834197ca1aa25b590123e644951b12e
13off <ilyaodincev1999@gmail.com>
2026-07-16 12:24:18 +0300

    Добавить общий сервис установки PWA

---
+++ lib/services/pwa_install_service.dart
@@ -0,0 +1,18 @@
+import 'pwa_install_service_stub.dart'
+    if (dart.library.html) 'pwa_install_service_web.dart' as implementation;
+
+class PwaInstallService {
+  const PwaInstallService._();
+
+  static bool get isSupported => implementation.isSupported;
+
+  static bool get isInstalled => implementation.isInstalled;
+
+  static bool get canPrompt => implementation.canPrompt;
+
+  static String get platformName => implementation.platformName;
+
+  static String get manualInstruction => implementation.manualInstruction;
+
+  static Future<String> install() => implementation.install();
+}
