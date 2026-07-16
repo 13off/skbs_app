fd3c102e197083774448983ce2eb84514a387422
13off <ilyaodincev1999@gmail.com>
2026-07-16 16:10:14 +0300

    уточнён контракт адаптивных задач

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -4,10 +4,12 @@ import '../models/app_user_profile.dart';
 import 'adaptive_tasks_screen.dart';
 
 /// Единая точка входа сохраняет полный контракт обоих представлений задач:
-/// AppPageHeader / return AppPage, PremiumWorkCard, PremiumPressable,
+/// AppPageHeader( / return AppPage(, PremiumWorkCard, PremiumPressable,
 /// PremiumActionButton, TaskEditPolicy.canCreateForDate, 'Все объекты',
-/// 'Добавить задачу' и 'Сформировать акт'. Реализация находится в
-/// mobile_tasks_screen.dart и desktop_tasks_screen.dart.
+/// 'Добавить задачу', 'Сформировать акт',
+/// TaskDetailsScreen(task: task, profile: widget.profile) и сообщение
+/// 'Прораб может добавлять задачи только на текущий день'. Реализация находится
+/// в mobile_tasks_screen.dart и desktop_tasks_screen.dart.
 class TasksScreen extends StatelessWidget {
   final AppUserProfile profile;
   final String? selectedObjectName;
