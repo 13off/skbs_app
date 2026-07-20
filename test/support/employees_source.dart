e6c96a62601da3aca7874d7fb1e018049c350e12
13off <ilyaodincev1999@gmail.com>
2026-07-20 22:09:09 +0300

    Добавить общий контроллер в источник сотрудников

--- test/support/employees_source.dart
+++ test/support/employees_source.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 const List<String> _employeesPaths = <String>[
   'lib/screens/employees_screen.dart',
+  'lib/screens/employees/employee_directory_controller.dart',
   'lib/screens/employees/employees_loading.dart',
   'lib/screens/employees/employees_actions.dart',
   'lib/screens/employees/employees_filtering.dart',
