827b64ba0dc87d32b0251a3fa26efa90ff76747c
13off <ilyaodincev1999@gmail.com>
2026-07-24 15:36:46 +0300

    Запустить финальную проверку управления колонками CRM

--- test/recruitment_column_management_contract_test.dart
+++ test/recruitment_column_management_contract_test.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter_test/flutter_test.dart';
 
 // Защищает единый сценарий управления колонками CRM на доске и в настройках.
+// Полный прогон подтверждает совместимость с существующими экранами AppСтрой.
 void main() {
   test('new CRM columns are explicitly placed at the right edge', () {
     final board = File(
