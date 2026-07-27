ebd33309bf22f3fad8702c1dc537663969e3cb29
13off <ilyaodincev1999@gmail.com>
2026-07-27 10:16:47 +0300

    Исправить формат срока MAX-подключения

--- lib/screens/employee_details_screen.dart
+++ lib/screens/employee_details_screen.dart
@@ -53,6 +53,12 @@ class _EmployeeDetailsScreenState extends State<EmployeeDetailsScreen> {
     setState(update);
   }
 
+  String formatDateTime(DateTime value) {
+    String twoDigits(int number) => number.toString().padLeft(2, '0');
+    return '${twoDigits(value.day)}.${twoDigits(value.month)}.${value.year} '
+        '${twoDigits(value.hour)}:${twoDigits(value.minute)}';
+  }
+
   @override
   void initState() {
     super.initState();
