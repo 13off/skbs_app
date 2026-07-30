0708a8e4190938914ba3bb4963d3a1124cf61500
13off <ilyaodincev1999@gmail.com>
2026-07-30 18:50:40 +0300

    подключена карта маршрутов сотрудника

--- lib/screens/employee_details_screen.dart
+++ lib/screens/employee_details_screen.dart
@@ -8,6 +8,7 @@ import '../data/employee_archive_repository.dart';
 import '../data/employee_repository.dart';
 import '../features/employee/data/employee_access_repository.dart';
 import '../features/employee/presentation/employee_professional_passport_viewer_screen.dart';
+import '../features/employee/presentation/employee_route_map_screen.dart';
 import '../features/tasks/presentation/employee_contribution_screen.dart';
 import '../models/app_user_profile.dart';
 import '../models/employee.dart';
