05ee11e9dbf193542c902e6f57c19015c8d5a609
13off <ilyaodincev1999@gmail.com>
2026-08-03 01:09:18 +0300

    chore: remove unused tools controller

--- lib/features/tools/data/company_tools_controller.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/foundation.dart';
-
-abstract final class CompanyToolsController {
-  static final ValueNotifier<int> revision = ValueNotifier<int>(0);
-
-  static void notifyChanged() {
-    revision.value += 1;
-  }
-}
