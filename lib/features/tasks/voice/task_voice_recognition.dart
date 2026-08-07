8a8c43701ca3e1516bef1130c4768b95d33564bb
13off <ilyaodincev1999@gmail.com>
2026-08-07 13:47:33 +0300

    голосовой ввод получает ручную остановку

--- lib/features/tasks/voice/task_voice_recognition.dart
+++ lib/features/tasks/voice/task_voice_recognition.dart
@@ -1,4 +1,7 @@
 import 'task_voice_recognition_native.dart'
     if (dart.library.js_interop) 'task_voice_recognition_web.dart' as platform;
 
-Future<String> recognizeTaskVoice() => platform.recognizeTaskVoice();
+Future<String> recognizeTaskVoice({List<String> hints = const <String>[]}) =>
+    platform.recognizeTaskVoice(hints: hints);
+
+Future<void> stopTaskVoiceRecognition() => platform.stopTaskVoiceRecognition();
