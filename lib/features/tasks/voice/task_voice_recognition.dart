574349b4b1081f097a492111c7fe1cc7e8229e1e
13off <ilyaodincev1999@gmail.com>
2026-08-07 15:20:36 +0300

    добавлен поток промежуточного текста голоса

--- lib/features/tasks/voice/task_voice_recognition.dart
+++ lib/features/tasks/voice/task_voice_recognition.dart
@@ -1,7 +1,9 @@
 import 'task_voice_recognition_native.dart'
     if (dart.library.js_interop) 'task_voice_recognition_web.dart' as platform;
 
-Future<String> recognizeTaskVoice({List<String> hints = const <String>[]}) =>
-    platform.recognizeTaskVoice(hints: hints);
+Future<String> recognizeTaskVoice({
+  List<String> hints = const <String>[],
+  void Function(String transcript)? onPartial,
+}) => platform.recognizeTaskVoice(hints: hints, onPartial: onPartial);
 
 Future<void> stopTaskVoiceRecognition() => platform.stopTaskVoiceRecognition();
