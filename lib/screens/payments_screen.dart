1498da58d19ed5d20e6a56e2905cbd1ba27cf35e
13off <ilyaodincev1999@gmail.com>
2026-08-14 12:15:18 +0300

    feat: показать неподтверждённые штрафы поверх выплат

--- lib/screens/payments_screen.dart
+++ lib/screens/payments_screen.dart
@@ -1,1 +1,34 @@
-export '../features/payments/presentation/screens/payments_screen.dart';
+import 'package:flutter/material.dart';
+
+import '../features/payments/presentation/screens/payments_screen.dart' as feature;
+import '../features/payments/presentation/widgets/pending_absence_fines_card.dart';
+
+class PaymentsScreen extends StatelessWidget {
+  final String? selectedObjectName;
+
+  const PaymentsScreen({super.key, this.selectedObjectName});
+
+  @override
+  Widget build(BuildContext context) {
+    final top = MediaQuery.paddingOf(context).top + kToolbarHeight + 8;
+
+    return Stack(
+      children: [
+        feature.PaymentsScreen(selectedObjectName: selectedObjectName),
+        Positioned(
+          top: top,
+          left: 16,
+          right: 16,
+          child: Center(
+            child: ConstrainedBox(
+              constraints: const BoxConstraints(maxWidth: 760, maxHeight: 420),
+              child: SingleChildScrollView(
+                child: PendingAbsenceFinesCard(),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
