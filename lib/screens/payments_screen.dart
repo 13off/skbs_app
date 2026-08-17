2642c8eacc266c71b0f70b283ae39bd925a4e812
13off <ilyaodincev1999@gmail.com>
2026-08-17 09:21:13 +0300

    style: привести режим учёта выплат к мобильному дизайну

--- lib/screens/payments_screen.dart
+++ lib/screens/payments_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../app/app_adaptive_palette.dart';
 import '../features/payments/presentation/screens/payments_screen.dart' as feature;
 
 class PaymentsScreen extends StatelessWidget {
@@ -9,6 +10,23 @@ class PaymentsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return feature.PaymentsScreen(selectedObjectName: selectedObjectName);
+    final theme = Theme.of(context);
+    final chipTheme = theme.chipTheme.copyWith(
+      backgroundColor: AppAdaptivePalette.surfaceElevated,
+      selectedColor: AppAdaptivePalette.accentSoft,
+      checkmarkColor: AppAdaptivePalette.accent,
+      side: BorderSide(color: AppAdaptivePalette.border),
+      shape: const StadiumBorder(),
+      padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 4),
+      labelStyle: TextStyle(
+        color: AppAdaptivePalette.textPrimary,
+        fontWeight: FontWeight.w800,
+      ),
+    );
+
+    return Theme(
+      data: theme.copyWith(chipTheme: chipTheme),
+      child: feature.PaymentsScreen(selectedObjectName: selectedObjectName),
+    );
   }
 }
