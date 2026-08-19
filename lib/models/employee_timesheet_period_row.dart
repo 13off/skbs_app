c67e9f1b72f13ebb154c5071b33f256929ee8e46
13off <ilyaodincev1999@gmail.com>
2026-08-19 14:58:59 +0300

    feat: add individual timesheet period model

---
+++ lib/models/employee_timesheet_period_row.dart
@@ -0,0 +1,26 @@
+import 'employee.dart';
+
+class EmployeeTimesheetPeriodRow {
+  final Employee employee;
+  final DateTime startDate;
+  final DateTime endDate;
+  final Map<String, double> shiftsByDate;
+  final double paid;
+
+  const EmployeeTimesheetPeriodRow({
+    required this.employee,
+    required this.startDate,
+    required this.endDate,
+    required this.shiftsByDate,
+    required this.paid,
+  });
+
+  double shiftForDate(String dateKey) => shiftsByDate[dateKey] ?? 0.0;
+
+  double get totalShifts =>
+      shiftsByDate.values.fold<double>(0.0, (sum, value) => sum + value);
+
+  double get accrued => totalShifts * employee.dailyRate;
+
+  double get balance => accrued - paid;
+}
