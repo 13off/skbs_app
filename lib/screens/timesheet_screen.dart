49237d04623cfccf4550ed06b69f7ae2a0a6c60d
13off <ilyaodincev1999@gmail.com>
2026-08-20 16:16:19 +0300

    Подключить группы к мобильному табелю

--- lib/screens/timesheet_screen.dart
+++ lib/screens/timesheet_screen.dart
@@ -11,18 +11,34 @@ import '../data/app_data_sync.dart';
 import '../data/app_state.dart';
 import '../data/attendance_repository.dart';
 import '../data/employee_repository.dart';
+import '../features/timesheet/data/timesheet_group_repository.dart';
 import '../features/timesheet/models/timesheet_draft.dart';
+import '../features/timesheet/models/timesheet_group.dart';
 import '../models/app_user_profile.dart';
 import '../models/employee.dart';
 import '../widgets/app_page.dart';
 import '../widgets/premium_ui.dart';
 import 'period_timesheet_screen.dart';
+import 'timesheet_group_manager_sheet.dart';
 
 part 'timesheet/timesheet_actions.dart';
 part 'timesheet/timesheet_loading.dart';
 part 'timesheet/timesheet_sections.dart';
 part 'timesheet/timesheet_sync.dart';
 part 'timesheet/timesheet_view.dart';
+
+const String _allTimesheetGroupsFilter = '__all__';
+const String _ungroupedTimesheetFilter = '__ungrouped__';
+
+class _TimesheetEmployeeGroupSection {
+  final String title;
+  final List<Employee> employees;
+
+  const _TimesheetEmployeeGroupSection({
+    required this.title,
+    required this.employees,
+  });
+}
 
 class TimesheetScreen extends StatefulWidget {
   final AppUserProfile profile;
@@ -42,8 +58,11 @@ class _TimesheetScreenState extends State<TimesheetScreen> {
   DateTime selectedDate = AppState.today;
   Future<List<Employee>>? employeesFuture;
   TimesheetDraft timesheetDraft = TimesheetDraft.empty();
+  List<TimesheetGroup> timesheetGroups = const <TimesheetGroup>[];
+  String selectedGroupFilter = _allTimesheetGroupsFilter;
 
   bool isAttendanceLoading = false;
+  bool isGroupsLoading = false;
   bool isSaving = false;
   String? errorText;
   bool hasPendingRemoteAttendance = false;
@@ -56,6 +75,10 @@ class _TimesheetScreenState extends State<TimesheetScreen> {
 
   bool get hasUnsavedChanges => timesheetDraft.hasChanges;
 
+  bool get canManageTimesheetGroups =>
+      widget.profile.actualRole == 'admin' ||
+      widget.profile.actualRole == 'developer';
+
   List<double> get allShiftOptions {
     return List<double>.generate(31, (index) => index / 10);
   }
@@ -64,6 +87,7 @@ class _TimesheetScreenState extends State<TimesheetScreen> {
   void initState() {
     super.initState();
     reloadEmployees();
+    loadTimesheetGroups();
     loadAttendance();
     dataChangeSubscription = AppDataSync.changes.listen(handleDataChange);
   }
@@ -72,7 +96,9 @@ class _TimesheetScreenState extends State<TimesheetScreen> {
   void didUpdateWidget(covariant TimesheetScreen oldWidget) {
     super.didUpdateWidget(oldWidget);
     if (oldWidget.selectedObjectName != widget.selectedObjectName) {
+      selectedGroupFilter = _allTimesheetGroupsFilter;
       reloadEmployees(forceRefresh: true);
+      loadTimesheetGroups(forceRefresh: true);
       loadAttendance(forceRefresh: true);
     }
   }
