dd3580d10b94212451c3414bc39b58a394438028
13off <ilyaodincev1999@gmail.com>
2026-08-31 19:04:14 +0300

    feat: show company branding after app startup

--- lib/screens/auth_gate.dart
+++ lib/screens/auth_gate.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/widgets.dart';
 
 import '../features/auth/presentation/offline_first_auth_gate.dart';
+import '../features/company/presentation/company_brand_splash_host.dart';
 import '../widgets/push_permission_prompt_host.dart';
 
 class AuthGate extends StatelessWidget {
@@ -8,8 +9,10 @@ class AuthGate extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const PushPermissionPromptHost(
-      child: OfflineFirstAuthGate(),
+    return const CompanyBrandSplashHost(
+      child: PushPermissionPromptHost(
+        child: OfflineFirstAuthGate(),
+      ),
     );
   }
 }
