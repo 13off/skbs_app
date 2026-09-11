8ee58c995050fa428c4e0ac10874f89714fe40ae
13off <ilyaodincev1999@gmail.com>
2026-09-12 00:11:44 +0300

    feat: place work plan before task assignees

--- lib/screens/task_create/task_create_view.dart
+++ lib/screens/task_create/task_create_view.dart
@@ -56,6 +56,8 @@ extension _TaskCreateView on _AddTaskScreenState {
           const SizedBox(height: 16),
           buildTaskFields(),
           const SizedBox(height: 16),
+          buildWorkPlanBlock(),
+          const SizedBox(height: 16),
           buildAssigneesBlock(),
           const SizedBox(height: 16),
           buildPhotosBlock(),
