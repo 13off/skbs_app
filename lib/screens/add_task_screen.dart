2238a4b11f9e8e8936bbaa6f34ea37546487911c
13off <ilyaodincev1999@gmail.com>
2026-09-12 21:12:42 +0300

    test: keep task creation shell within structure budget

--- lib/screens/add_task_screen.dart
+++ lib/screens/add_task_screen.dart
@@ -25,7 +25,6 @@ part 'task_create/task_create_loading.dart';
 part 'task_create/task_create_sections.dart';
 part 'task_create/task_create_view.dart';
 part 'task_create/task_create_voice.dart';
-
 class TaskCreateDraft {
   final TaskItemData task;
   final List<String> assigneeIds;
